0383697b0662002d87f37138a9baff04f748abe6
chen_1335 <3388508783@qq.com>
2025-12-29 17:26:02 +0800

    旧版辣椒转换机制移除

--- src/main/java/com/bmt/kaleidoscope_nether/event/SoulPepperTransformationHandler.java
+++ src/main/java/com/bmt/kaleidoscope_nether/event/SoulPepperTransformationHandler.java
@@ -1,51 +1,12 @@
 package com.bmt.kaleidoscope_nether.event;
 
-import com.bmt.kaleidoscope_nether.API.KNTags;
-import com.bmt.kaleidoscope_nether.KaleidoscopeNether;
-import com.bmt.kaleidoscope_nether.registry.KNItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = KaleidoscopeNether.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class SoulPepperTransformationHandler {
-
-    @SubscribeEvent
-    public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
-        Player player = event.getEntity();
-        Level level = event.getLevel();
-        BlockPos pos = event.getPos();
-        BlockState blockState = level.getBlockState(pos);
-        ItemStack heldItem = player.getMainHandItem();
-
-        if (blockState.is(KNTags.Blocks.SOUL_SOIL_SAND)) {
-            if (heldItem.is(KNTags.Items.SOUL_PEPPER_TRANSFORMABLE)) {
-                if (!player.isCreative()) {
-                    heldItem.shrink(1);
-                }
-
-                ItemStack soulPepper = new ItemStack(KNItems.SOUL_PEPPER.get());
-                if (!player.getInventory().add(soulPepper)) {
-                    player.drop(soulPepper, false);
-                }
-
-                if (!level.isClientSide()) {
-                    spawnSoulParticles(level, pos, player);
-                }
-
-                event.setCanceled(true);
-                event.setCancellationResult(InteractionResult.SUCCESS);
-            }
-        }
-    }
 
     private static void spawnSoulParticles(Level level, BlockPos pos, Player player) {
         if (level instanceof ServerLevel serverLevel) {
