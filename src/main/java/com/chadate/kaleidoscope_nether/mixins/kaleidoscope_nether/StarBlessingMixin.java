971c579d8b944316c641a5537937e924df4c0aa3
BmtUltra <1453935889@qq.com>
2026-02-17 19:57:56 +0800

    依旧mixin

--- src/main/java/com/chadate/kaleidoscope_nether/mixins/kaleidoscope_nether/StarBlessingMixin.java
+++ src/main/java/com/chadate/kaleidoscope_nether/mixins/kaleidoscope_nether/StarBlessingMixin.java
@@ -21,18 +21,12 @@ public abstract class StarBlessingMixin extends Entity {
         super(entityType, level);
     }
 
-    /**
-     * 免疫debuff：阻止负面效果的应用
-     */
     @Inject(method = "addEffect(Lnet/minecraft/world/effect/MobEffectInstance;Lnet/minecraft/world/entity/Entity;)Z", 
             at = @At("HEAD"), cancellable = true)
     private void onAddEffect(MobEffectInstance effectInstance, Entity entity, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity livingEntity = (LivingEntity) (Object) this;
-        
         if (livingEntity.hasEffect(KNEffects.STAR_BLESSING)) {
             MobEffect effect = effectInstance.getEffect().value();
-            
-            // 如果是负面效果，阻止其应用
             if (effect.getCategory() == MobEffectCategory.HARMFUL) {
                 cir.setReturnValue(false);
                 cir.cancel();
@@ -40,29 +34,19 @@ public abstract class StarBlessingMixin extends Entity {
         }
     }
 
-    /**
-     * 免疫击退：取消击退效果
-     */
     @Inject(method = "knockback", at = @At("HEAD"), cancellable = true)
     private void onKnockback(double strength, double x, double z, CallbackInfo ci) {
         LivingEntity livingEntity = (LivingEntity) (Object) this;
-        
         if (livingEntity.hasEffect(KNEffects.STAR_BLESSING)) {
-            ci.cancel(); // 取消击退
+            ci.cancel();
         }
     }
 
-    /**
-     * 免疫击退：在受伤时也取消击退
-     */
     @Inject(method = "hurt", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;knockback(DDD)V"), 
             cancellable = true)
     private void onHurtKnockback(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity livingEntity = (LivingEntity) (Object) this;
-        
         if (livingEntity.hasEffect(KNEffects.STAR_BLESSING)) {
-            // 不取消伤害，只取消击退
-            // 这里我们让原方法继续执行，但会在上面的knockback方法中取消击退
         }
     }
 }
