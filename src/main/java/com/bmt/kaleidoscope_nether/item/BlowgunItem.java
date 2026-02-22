3393c3bc6defc761eb95c7786bb8b1adc2ebd386
BmtUltra <1453935889@qq.com>
2026-02-22 11:05:27 +0800

    汤底

--- src/main/java/com/bmt/kaleidoscope_nether/item/BlowgunItem.java
+++ src/main/java/com/bmt/kaleidoscope_nether/item/BlowgunItem.java
@@ -15,7 +15,7 @@ import java.util.function.Predicate;
 
 public class BlowgunItem extends ProjectileWeaponItem {
     public static final int MAX_DRAW_DURATION = 7;
-    public static final float DEFAULT_POWER = 3.0F;
+    //public static final float DEFAULT_POWER = 3.0F;
 
     public BlowgunItem(Properties properties) {
         super(properties);
